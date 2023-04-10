a9241867b285a108397ba8df324c90e7c3c59853
meharsh05 <me.harshmehra07@gmail.com>
2023-04-10 18:35:41 +0530

    up

--- basic.cpp
+++ basic.cpp
@@ -109,3 +109,53 @@ else{
     
     return 0;
 }
+
+/*
+SWITCH STATEMENT 
+Ex. 4 - Take the day number and print the corresponding day
+for 1 print "Monday", for 2 print "Tuesday" and so on
+for 7 print "Sunday". 
+*/
+#include<bits/stdc++.h>
+using namespace std; 
+
+int main(){
+    int day;
+    cout << "Enter the day no. : ";
+    cin >> day; 
+
+switch(day){
+    case 1:
+        cout << "Monday" << endl;
+        break;
+
+    case 2:
+        cout << "Tuesday" << endl;
+        break;
+
+    case 3:
+        cout << "Wednesday" << endl;
+        break;
+
+    case 4:
+        cout << "Thursday" << endl;
+        break;
+
+    case 5:
+        cout << "Friday" << endl;
+        break;
+
+    case 6:
+        cout << "Saturday" << endl;
+        break;
+
+    case 7:
+        cout << "Sunday" << endl;
+        break;
+
+    default:
+        cout << "Invalid" << endl;
+
+}
+    return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -2,22 +2,42 @@
 using namespace std; 
 
 int main(){
-    int age;
-    cout << "Enter Your Age: ";
-    cin >> age;
+    int day;
+    cout << "Enter the day no. : ";
+    cin >> day; 
 
-if(age < 18){
-    cout << "Not eligible for job" << endl;
+switch(day){
+    case 1:
+        cout << "Monday" << endl;
+        break;
+
+    case 2:
+        cout << "Tuesday" << endl;
+        break;
+
+    case 3:
+        cout << "Wednesday" << endl;
+        break;
+
+    case 4:
+        cout << "Thursday" << endl;
+        break;
+
+    case 5:
+        cout << "Friday" << endl;
+        break;
+
+    case 6:
+        cout << "Saturday" << endl;
+        break;
+
+    case 7:
+        cout << "Sunday" << endl;
+        break;
+
+    default:
+        cout << "Invalid" << endl;
+
 }
-else if(age <= 54){
-    cout << "Eligible for job" << endl;
-}
-else if(age <= 57){
-    cout << "Eligible for job but retirement soon" << endl;
-}
-else{
-    cout << "Retirement Soon" << endl;
-}
-    
     return 0;
 }
