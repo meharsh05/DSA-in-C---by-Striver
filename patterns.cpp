7fd8dcb7c1f561207fa7316810a8274fcf758352
meharsh05 <me.harshmehra07@gmail.com>
2023-04-12 22:40:05 +0530

    up

--- patterns.cpp
+++ patterns.cpp
@@ -138,8 +138,8 @@ Pattern 5-
 using namespace std; 
 
 void print5(int n){
-    for(int i=1; i<=n; i++){
-        for(int j=0; j<n-i+1; j++){
+    for(int i=0; i<n; i++){
+        for(int j=1; j<n-i+1; j++){
             cout << "* ";
         }
         cout << endl;
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -2,8 +2,8 @@
 using namespace std; 
 
 void print5(int n){
-    for(int i=1; i<=n; i++){
-        for(int j=0; j<n-i+1; j++){
+    for(int i=0; i<n; i++){
+        for(int j=1; j<n-i+1; j++){
             cout << "* ";
         }
         cout << endl;
