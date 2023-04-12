9ef6bc796fc43ae79a2303b1ba08f736bc5189da
meharsh05 <me.harshmehra07@gmail.com>
2023-04-12 19:02:58 +0530

    up

--- patterns.cpp
+++ patterns.cpp
@@ -58,3 +58,38 @@ int main(){
         print2(n);
     }
 }
+
+/*
+Pattern 3 - 1
+            1 2
+            1 2 3
+            1 2 3 4
+            1 2 3 4 5
+*/
+#include<iostream>
+using namespace std; 
+
+void print3(int n){
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout << j;
+        }
+        cout << endl;
+    }
+}
+int main(){
+    int t;
+    cout << "Enter the value for test cases:";
+    cin>>t;
+
+    for(int i=0; i<t; i++){
+        int n;
+        cin>>n;
+        print3(n);
+    }
+    
+}
+
+/*
+Pattern 4 - 
+*/
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,22 +1,23 @@
 #include<iostream>
 using namespace std; 
 
-void print2(int n){
+void print3(int n){
     for(int i=1; i<=n; i++){
         for(int j=1; j<=i; j++){
-            cout << " * ";
+            cout << j;
         }
         cout << endl;
     }
 }
 int main(){
     int t;
-    cout << "Enter the value for test case to be run: ";
+    cout << "Enter the value for test cases:";
     cin>>t;
 
     for(int i=0; i<t; i++){
         int n;
         cin>>n;
-        print2(n);
+        print3(n);
     }
+    
 }
