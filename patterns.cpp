ef56ed690855bc4d492592dec850acb54d208506
meharsh05 <me.harshmehra07@gmail.com>
2023-04-14 16:35:09 +0530

    up

--- patterns.cpp
+++ patterns.cpp
@@ -270,10 +270,72 @@ void print8(int n){
 int main(){
     int t;
     cout << "Enter the value for test cases: ";
+    cin>>t;
 
     for(int i=0; i<t; i++){
         int n;
         cin>>n;
+       
         print8(n);
     }
 }
+
+/*
+Pattern 9 - 
+                *               
+               ***
+              *****          
+             *******
+            *********
+            *********
+             *******
+              *****
+               ***
+                *
+*/
+
+#include<iostream>
+using namespace std; 
+
+void printa(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n-i-1; j++){
+            cout << " ";
+        }
+        for(int j=0; j<2*i+1; j++){
+            cout << "*";
+        }
+        for(int j=0; j<n-i-1; j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+void printb(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i; j++){
+            cout << " ";
+        }
+        for(int j=0; j<2*n-(2*i+1); j++){
+            cout << "*";
+        }
+        for(int j=0; j<i; j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    int t;
+    cout << "Enter the value for test cases: ";
+    cin>>t;
+
+    for(int i=0; i<t; i++){
+        int n;
+        cin>>n;
+        printa(n);
+        printb(n);
+    }
+}
+
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,7 +1,21 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std; 
 
-void print8(int n){
+void printa(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n-i-1; j++){
+            cout << " ";
+        }
+        for(int j=0; j<2*i+1; j++){
+            cout << "*";
+        }
+        for(int j=0; j<n-i-1; j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+void printb(int n){
     for(int i=0; i<n; i++){
         for(int j=0; j<i; j++){
             cout << " ";
@@ -19,10 +33,12 @@ void print8(int n){
 int main(){
     int t;
     cout << "Enter the value for test cases: ";
+    cin>>t;
 
     for(int i=0; i<t; i++){
         int n;
         cin>>n;
-        print8(n);
+        printa(n);
+        printb(n);
     }
 }
